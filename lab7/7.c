ca8d9e7b761008fced67252d362302b1a22e567d
Shashwat Rai <shashwatrai_28@outlook.com>
2026-09-26 09:22:48 +0530

    e

--- lab7/7.c
+++ lab7/7.c
@@ -12,7 +12,7 @@ int main() {
     }
 
     for (int i = 0; i < input - 1; i++) {
-        for (int k = 0; k < i - 1; k++) {
+        for (int k = 0; k < input - i - 1; k++) {
             if (array[k] > array[k + 1]) {
                 array[k] ^= array[k + 1];
                 array[k + 1] ^= array[k];
